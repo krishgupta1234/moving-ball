4175a572c354fcdc147500ecd9f802c0f28d3129
krishgupta1234 <75523301+krishgupta1234@users.noreply.github.com>
2021-07-07 15:00:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var movingball;
+var database;
+var position;
+
+function setup() {
+    database = firebase.database();
+    console.log(database)
+    createCanvas(500, 500);
+    movingball = createSprite(250, 250, 10, 10);
+    movingball.shapeColor = "red";
+
+    database.ref("ball/position").on("value", readPosition, showError);
+}
+
+function draw() {
+    background("white");
+    if (keyDown(LEFT_ARROW)) {
+        changePosition(-1, 0);
+    }
+    else if (keyDown(RIGHT_ARROW)) {
+        changePosition(1, 0);
+    }
+    else if (keyDown(UP_ARROW)) {
+        changePosition(0, -1);
+    }
+    else if (keyDown(DOWN_ARROW)) {
+        changePosition(0, +1);
+    }
+    drawSprites();
+}
+
+function changePosition(x, y) {
+    database.ref("ball/position").set({
+        x: position.x + x,
+        y: position.y + y
+    })
+    //movingball.x = movingball.x + x;
+    //movingball.y = movingball.y + y;
+}
+
+function readPosition(data) {
+    position = data.val();
+    console.log(position.x);
+    movingball.x = position.x;
+    movingball.y = position.y;
+}
+
+
+function showError() {
+    console.log("Error in writing to the database");
+}
+
+
+
